cd3f5a1a146171625d6f5dd5fbf0838b782dbb77
verdie90 <theferdianz@gmail.com>
2023-04-25 13:42:31 +0700

    Kom3 dengan db

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,48 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import session, { Cookie } from "express-session";
+import dotenv from "dotenv";
+import db from "./config/Database.js";
+import SequelizeStore from "connect-session-sequelize";
+import AuthRoute from "./routes/auth/AuthRoute.js";
+import UserRoute from "./routes/users/UserRoute.js";
+
+dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+const sessionStore = SequelizeStore(session.Store);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const store = new sessionStore({
+    db : db,
 });
 
-app.listen(5000, () => {
-    console.log('Listening on port 5000');
-})
+// (async()=>{
+//     await db.sync();
+// })();
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+        secure : 'auto',
+    }
+}));
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(AuthRoute);
+app.use(UserRoute);
+
+// store.sync();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+});
